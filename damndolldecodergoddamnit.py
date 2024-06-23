d712ddea5f5c4d68b7ac85e3a7777ea57c1e7ca6
J Fidel <jjfidel505@gmail.com>
2024-06-23 12:37:26 -0600

    removed debugging tools

--- damndolldecodergoddamnit.py
+++ damndolldecodergoddamnit.py
@@ -15,7 +15,6 @@ chain = []
 #main
 
 for o in operand:
-    #print("Highest Level:" + str(list(o)))
     decode = list(o)
     for d in decode:
         if(d == "▌"):
@@ -31,14 +30,11 @@ for o in operand:
     p = 0
     dec = 0
     for f in fixedblock:
-        #print(f)
         dec += (3 ** p)*int(f)
         p += 1
-    #print(dec)
-    result.append(chr(dec))
 
-    #print("Raw Block:" + str(block))
-    #print("Fixed Block:" + str(fixedblock))
+    result.append(chr(dec))
+    
     block.clear()
     fixedblock.clear()
 print("".join(result))
