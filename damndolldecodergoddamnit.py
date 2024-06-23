eeb7b96734816d5dc445fde015a571dd7fe4eff4
Moonrise <aliuesch@gmail.com>
2024-06-23 12:35:35 -0600

    removed superfluous functions

--- damndolldecodergoddamnit.py
+++ damndolldecodergoddamnit.py
@@ -13,36 +13,7 @@ operand = userput.split(" ")
 
 block = []
 chain = []
-
-#functions
-def charToDecimal(characterInput):
-
-    dec = int(format(ord(characterInput), "x"), 16)
-
-    return dec
-
-def decToDollcode(userInput):
-
-    workingOutput = []
-
-    mult = (charToDecimal(userInput))
-
-    while mult >= 1:
-
-        mod = mult % 3
-
-        if mod == 0 :
-
-            mult = (mult - 3) / 3
-        
-        else: 
-            
-            mult = (mult - mod) / 3
-
-        workingOutput.append(charmap[int(mod)])
-    
-    return "".join(reversed(workingOutput))
-            
+       
 #main
 
 #print(operand)
